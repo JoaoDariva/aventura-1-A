b2842dd6b63781e45e57b7ff9442dd63fdddb8b9
João Pedro Nardoni Dariva <joao.dariva@escola.pr.gov.br>
2025-09-16 08:06:13 -0300

    hoje

---
+++ script.js
@@ -0,0 +1,15 @@
+const avanca = document.querySelectorAll('.btn-proximo')
+
+avanca.forEach(buttton => {
+   buttton.addEventListener('click', function (){
+const atual = document.querySelector('ativo');
+const
+   }
+
+
+
+
+   )
+}
+
+)
